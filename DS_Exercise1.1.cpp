cd07ae32fa21c13d5b3b853fd30c47092d76d3e6
Seán Smith <seansmith1994@gmail.com>
2019-04-11 00:03:32 +0100

    Update DS_Exercise1.1.cpp

--- DS_Exercise1.1.cpp
+++ DS_Exercise1.1.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
   // initialise MPI 
   MPI_Init(NULL, NULL);
   
-  //World size and ranl
+  //World size and rank
   int world_size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
